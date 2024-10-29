1bc49356e1dac78329ebaa2f12bdc554f55dcd89
sombitganguly <sgrt.gly@gmail.com>
2024-10-30 03:23:15 +0530

    created user schema and sign-up authentication api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from './routes/auth.route.js'
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
     console.log("Server is running at port 3000");
 });
+
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
