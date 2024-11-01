9c65377992f3105eb4bcbaf8c90d4dac1dd95230
sombitganguly <sgrt.gly@gmail.com>
2024-11-02 00:03:36 +0530

    created add listings page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,7 @@ import {
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -100,6 +101,9 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
       <span
