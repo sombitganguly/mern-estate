95322b50e8c2818051dfdb103995f7de4438ef44
sombitganguly <sgrt.gly@gmail.com>
2024-11-03 12:54:13 +0530

    fixed server issues

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup', {
+      const res = await fetch('https://sombit-estate-backend.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
