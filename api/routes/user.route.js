f5d185c2cc05016fe24dbd1e2dc140deb713b3f3
sombitganguly <sgrt.gly@gmail.com>
2024-11-11 01:58:38 +0530

    fixed minor code

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,7 +6,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
+router.delete('/delete/:id', deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
 router.get('/:id', verifyToken, getUser)
 
